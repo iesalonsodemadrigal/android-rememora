8066bc0394aca04c6e31eafe8e0224233e21e515
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-11-08 00:02:04 +0100

    estructura de fragmentos (#15)

---
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.iesam.rememora.features.home.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iesam.rememora.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
