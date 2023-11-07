8c4f3c1f04c3e5c6707be8af2f3b5319276933ca
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2023-11-07 12:32:52 +0100

    Añadidas librerias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,28 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    //ViewBinding
+    buildFeatures{
+        viewBinding=true
+    }
 }
 
 dependencies {
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    //Skeleton
+    implementation ("com.faltenreich:skeletonlayout:5.0.0")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Loggin interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    //Gson
+    implementation ("com.google.code.gson:gson:2.10.1")
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
