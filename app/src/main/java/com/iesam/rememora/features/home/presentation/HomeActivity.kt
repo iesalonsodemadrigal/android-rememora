3b064e3ae88c42a231b5ecec9a2b4b35cd3f1e4c
sofia <sofia.jimlap@educa.jcyl.es>
2023-11-08 19:50:41 +0100

    pantalla principal con fragment y navigation

--- app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -2,11 +2,35 @@ package com.iesam.rememora.features.home.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.iesam.rememora.R
+import com.iesam.rememora.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private var _binding : ActivityHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+    }
+    private fun setupView(){
+        binding.apply {
+            actionImage.setOnClickListener{
+                it.findNavController().navigate(R.id.fragment_imagen)
+            }
+            actionVideo.setOnClickListener {
+                it.findNavController().navigate(R.id.fragment_video)
+            }
+            actionMusica.setOnClickListener {
+                it.findNavController().navigate(R.id.fragment_music)
+            }
+            actionAudio.setOnClickListener {
+                it.findNavController().navigate(R.id.action_audio)
+            }
+        }
     }
 }
