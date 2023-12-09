95d4c44ebbcf681fd57ee80bc74ad6585c337844
Cjr37 <carlosclase37@gmail.com>
2023-12-09 19:17:17 +0100

    feature: Dependencias Firebase y Realtime
    creacion flujo parte domain

--- app/src/main/java/com/iesam/rememora/features/images/domain/GetImagesUseCase.kt
+++ app/src/main/java/com/iesam/rememora/features/images/domain/GetImagesUseCase.kt
@@ -1,4 +1,10 @@
 package com.iesam.rememora.features.images.domain
 
-class GetImagesUseCase {
+import com.iesam.rememora.app.Either
+import com.iesam.rememora.app.domain.ErrorApp
+
+class GetImagesUseCase(private val repository: ImageRepository) {
+    suspend operator fun invoke(): Either<ErrorApp, List<Image>> {
+        return repository.getImages()
+    }
 }
--- app/src/main/java/com/iesam/rememora/features/images/domain/Image.kt
+++ app/src/main/java/com/iesam/rememora/features/images/domain/Image.kt
@@ -1,4 +1,8 @@
 package com.iesam.rememora.features.images.domain
 
-class Image {
-}
+data class Image(
+    val description: String? = null,
+    val id: Int = 0,
+    var source: String? = null,
+    val title: String? = null
+)
