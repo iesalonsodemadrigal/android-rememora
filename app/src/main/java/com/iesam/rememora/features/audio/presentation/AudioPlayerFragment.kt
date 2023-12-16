fffe75cc6e41c9e19d3f8af0c71fdb7f9e9e2e5c
Sofia <sofia.jimlap@educa.jcyl.es>
2023-12-16 17:42:15 +0100

    feature 26: listen audios Añadido custom view

--- app/src/main/java/com/iesam/rememora/features/audio/presentation/AudioPlayerFragment.kt
+++ app/src/main/java/com/iesam/rememora/features/audio/presentation/AudioPlayerFragment.kt
@@ -1,6 +1,5 @@
 package com.iesam.rememora.features.audio.presentation
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.iesam.rememora.app.domain.ErrorApp
 import com.iesam.rememora.databinding.FragmentAudioBinding
-import com.iesam.rememora.features.audio.domain.Audio
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,47 +17,15 @@ class AudioPlayerFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel by viewModels<AudioPlayerViewModel>()
 
-    private var audiosList : List<Audio> = listOf( )
-
-    private lateinit var player: SimpleExoPlayer
-    private var audioIndex = 0
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAudioBinding.inflate(inflater, container, false)
-        setupView()
         return binding.root
     }
 
-    private fun setupView() {
-        binding.mediaControls.apply {
-            backButton.setOnClickListener{
-                playPreviousSong()
-            }
-            nextButton.setOnClickListener{
-                playNextSong()
-            }
-            repeatButton.setOnClickListener{
-                prepareAndPlaySong(audiosList[audioIndex].source!!)
-            }
-        }
-    }
-    private fun playPreviousSong() {
-        if (audioIndex > 0) {
-            audioIndex--
-        }
-        prepareAndPlaySong(audiosList[audioIndex].source!!)
-    }
-    private fun playNextSong() {
-        if (audioIndex < audiosList.size - 1) {
-            audioIndex++
-        }
-        prepareAndPlaySong(audiosList[audioIndex].source!!)
-    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObserver()
@@ -76,9 +40,11 @@ class AudioPlayerFragment : Fragment() {
                 if (it.errorApp != null) {
                     showError(it.errorApp)
                 } else {
-                    it.audios?.apply {
-                        audiosList = this
-                        initializePlayer()
+                    it.audios?.let { audios ->
+                        val urlListAudios : List<String> = audios.map { audio ->
+                            audio.source!!
+                        }
+                        binding.mediaPlayer.render(urlListAudios)
                     }
                 }
             }
@@ -89,25 +55,8 @@ class AudioPlayerFragment : Fragment() {
     private fun showError(error: ErrorApp) {
     }
 
-    private fun initializePlayer() {
-        player = SimpleExoPlayer.Builder(requireContext()).build()
-        binding.playerView.player = player
-        prepareAndPlaySong(audiosList[audioIndex].source!!)
-    }
-    private fun prepareAndPlaySong(songUrl: String) {
-        player.stop()
-        player.clearMediaItems()
-
-        val mediaItem = MediaItem.fromUri(Uri.parse(songUrl))
-        player.setMediaItem(mediaItem)
-        player.prepare()
-        player.play()
-        binding.audioName.text = audiosList[audioIndex].title
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        player.release()
     }
 }
