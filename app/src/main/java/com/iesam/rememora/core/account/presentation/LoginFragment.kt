f8c7642ea08ec21deb51b3749fc8f945fc5a1fce
Cjr37 <carlosclase37@gmail.com>
2023-12-16 20:23:23 +0100

    feature: Login con google
    corregida funcion para salir de la aplicacion

--- app/src/main/java/com/iesam/rememora/core/account/presentation/LoginFragment.kt
+++ app/src/main/java/com/iesam/rememora/core/account/presentation/LoginFragment.kt
@@ -1,17 +1,16 @@
 package com.iesam.rememora.core.account.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.iesam.rememora.databinding.FragmentLoginBinding
-import com.iesam.rememora.features.home.presentation.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -40,6 +39,7 @@ class LoginFragment : Fragment() {
                 createSignInIntent()
             }
             buttonCancel.setOnClickListener {
+                requireActivity().finish()
             }
         }
     }
@@ -57,9 +57,7 @@ class LoginFragment : Fragment() {
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         if (result.resultCode == AppCompatActivity.RESULT_OK) {
-            val intent = Intent(activity, HomeActivity::class.java)
-            startActivity(intent)
-            //findNavController().navigate(LoginFragmentDirections.fromLoginToLogout())
+            findNavController().navigate(LoginFragmentDirections.actionFromLoginToHome())
         } else {
         }
     }
--- app/src/main/java/com/iesam/rememora/core/account/presentation/LogoutFragment.kt
+++ app/src/main/java/com/iesam/rememora/core/account/presentation/LogoutFragment.kt
@@ -1,6 +1,5 @@
 package com.iesam.rememora.core.account.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.iesam.rememora.app.domain.ErrorApp
 import com.iesam.rememora.databinding.FragmentLogoutBinding
-import com.iesam.rememora.features.home.presentation.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -39,8 +38,7 @@ class LogoutFragment : Fragment() {
     }
 
     private fun init() {
-        val intent = Intent(activity, HomeActivity::class.java)
-        startActivity(intent)
+        findNavController().navigate(LogoutFragmentDirections.actionFromLogoutToLogin())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
