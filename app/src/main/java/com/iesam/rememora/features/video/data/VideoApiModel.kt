9b09482c104c26581f73c4edaf244247e374a558
diegogd04 <diego2612004@gmail.com>
2023-12-19 15:25:52 +0100

    feature: Visualización de vídeos
    Cambios comentarios pull request

--- app/src/main/java/com/iesam/rememora/features/video/data/VideoApiModel.kt
+++ app/src/main/java/com/iesam/rememora/features/video/data/VideoApiModel.kt
@@ -5,9 +5,9 @@ import com.iesam.rememora.features.video.domain.Video
 data
 class VideoApiModel(
     val description: String? = null,
-    val id: Int? = null,
+    val id: String? = null,
     var source: String? = null,
     val title: String? = null
 )
 
-fun VideoApiModel.toModel(): Video = Video(this.description, this.id.toString(), this.source, this.title)
+fun VideoApiModel.toModel(): Video = Video(this.description, this.id, this.source, this.title)
