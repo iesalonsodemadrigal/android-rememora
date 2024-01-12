dfe4072c0ce8fd6d254c4667ee10e6101bb80d63
Cjr37 <carlosclase37@gmail.com>
2024-01-12 15:17:46 +0100

    feature: room en Imagenes
    Creado room en imagenes e implementado

---
+++ app/src/main/java/com/iesam/rememora/features/images/data/local/DbMappers.kt
@@ -0,0 +1,9 @@
+package com.iesam.rememora.features.images.data.local
+
+import com.iesam.rememora.features.images.domain.Image
+
+fun Image.toEntity(ms: Long): ImageEntity =
+    ImageEntity(this.id, this.title, this.description, this.source, ms)
+
+fun ImageEntity.toDomain(): Image =
+    Image(this.description, this.uid, this.source, this.title)
---
+++ app/src/main/java/com/iesam/rememora/features/images/data/local/ImageDao.kt
@@ -0,0 +1,16 @@
+package com.iesam.rememora.features.images.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ImageDao {
+
+    @Query("SELECT * FROM imageEntity")
+    suspend fun getAll(): List<ImageEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg image: ImageEntity)
+}
