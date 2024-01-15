f46185e6646329045cf59aa1c19d2cbaba78c47e
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-01-15 01:47:36 +0100

    Implementar Room en audios (#55)

--- app/src/main/java/com/iesam/rememora/features/audio/data/AudioDataRepository.kt
+++ app/src/main/java/com/iesam/rememora/features/audio/data/AudioDataRepository.kt
@@ -2,12 +2,22 @@ package com.iesam.rememora.features.audio.data
 
 import com.iesam.rememora.app.Either
 import com.iesam.rememora.app.domain.ErrorApp
+import com.iesam.rememora.features.audio.data.local.AudioLocalDataSource
+import com.iesam.rememora.features.audio.data.remote.AudioRemoteDataSource
 import com.iesam.rememora.features.audio.domain.Audio
 import com.iesam.rememora.features.audio.domain.AudiosRepository
 import javax.inject.Inject
 
-class AudioDataRepository @Inject constructor (private val remoteDataSource: AudioRemoteDataSource) : AudiosRepository {
+class AudioDataRepository @Inject constructor (private val remoteDataSource : AudioRemoteDataSource, private val localDataSource : AudioLocalDataSource) : AudiosRepository {
     override suspend fun getAudios(uid: String): Either<ErrorApp, List<Audio>> {
-        return remoteDataSource.getAudios(uid)
+        val localAudios = localDataSource.obtainAudios()
+        return if (localAudios.isRight() && localAudios.get().isNotEmpty()) localAudios
+        else{
+            return remoteDataSource.getAudios(uid).map { audios ->
+                localDataSource.deleteAllAudios()
+                localDataSource.saveAudios(audios)
+                audios
+            }
+        }
     }
 }
