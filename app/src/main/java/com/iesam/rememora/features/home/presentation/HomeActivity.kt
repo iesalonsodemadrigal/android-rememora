38b126ea0c5700d32ac42a93062f095ceff742e9
diegogd04 <diego2612004@gmail.com>
2024-02-06 17:00:59 +0100

    feature: Crear control de volumen
    Crear botones en la vista e implementar su funcionalidad

--- app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.iesam.rememora.features.home.presentation
 
+import android.content.Context
+import android.media.AudioManager
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -17,16 +20,31 @@ class HomeActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<HomeViewModel>()
 
+    private lateinit var audioManager: AudioManager
+    private lateinit var volumeDownBottom: Button
+    private lateinit var volumeUpBottom: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        volumeDownBottom = findViewById(R.id.volume_down)
+        volumeUpBottom = findViewById(R.id.volume_up)
+
         setupView()
         setupObserver()
     }
 
     private fun setupView() {
         binding.apply {
+            volumeDownBottom.setOnClickListener{
+                volumeDown()
+            }
+            volumeUpBottom.setOnClickListener{
+                volumeUp()
+            }
             actionImage.setOnClickListener {
                 Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
                     .navigate(R.id.fragment_imagen)
@@ -65,5 +83,18 @@ class HomeActivity : AppCompatActivity() {
             }
         }
         viewModel.uiState.observe(this, observer)
+    }
+
+    private fun volumeDown(){
+        val volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        val newVolume = maxOf(volume - 1, 0)
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0)
+    }
+
+    private fun volumeUp(){
+        val volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        val newVolume = minOf(volume + 1, maxVolume)
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0)
     }
 }
