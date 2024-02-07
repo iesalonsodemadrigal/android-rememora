e1e177fed395358e9bb5df976225e4879ebc5a24
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-02-07 16:42:57 +0100

    feature 63: mock repository audios

--- app/src/main/java/com/iesam/rememora/features/audio/domain/GetAudiosUseCase.kt
+++ app/src/main/java/com/iesam/rememora/features/audio/domain/GetAudiosUseCase.kt
@@ -2,22 +2,13 @@ package com.iesam.rememora.features.audio.domain
 
 import com.iesam.rememora.app.Either
 import com.iesam.rememora.app.domain.ErrorApp
-import com.iesam.rememora.app.left
-import com.iesam.rememora.core.account.domain.AccountRepository
 import javax.inject.Inject
 
 class GetAudiosUseCase @Inject constructor(
-    private val repository: AudiosRepository,
-    private val accountRepository: AccountRepository
+    private val repository: AudiosRepository
 ) {
     suspend operator fun invoke(): Either<ErrorApp, List<Audio>> {
-        val user = accountRepository.getAccount()
-        return user.map { userDb ->
-            return if (userDb != null) {
-                repository.getAudios(userDb.id!!)
-            } else {
-                ErrorApp.SessionError.left()
-            }
-        }
+        return repository.getAudios()
+
     }
 }
