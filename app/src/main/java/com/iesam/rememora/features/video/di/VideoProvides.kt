c11ba7de9731116f6d285df1647542b63c7945a8
diegogd04 <diego2612004@gmail.com>
2024-02-07 22:21:34 +0100

    feature: Añadir MockRepository en vídeos
    Crear datos y modificar funciones para obtenerlos

--- app/src/main/java/com/iesam/rememora/features/video/di/VideoProvides.kt
+++ app/src/main/java/com/iesam/rememora/features/video/di/VideoProvides.kt
@@ -1,7 +1,11 @@
 package com.iesam.rememora.features.video.di
 
+import com.iesam.rememora.BuildConfig
 import com.iesam.rememora.app.data.local.AppDatabase
+import com.iesam.rememora.features.video.data.VideoDataRepository
 import com.iesam.rememora.features.video.data.local.VideoDao
+import com.iesam.rememora.features.video.data.mock.VideoMockDataRepository
+import com.iesam.rememora.features.video.domain.VideoRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +16,16 @@ import dagger.hilt.android.components.ViewModelComponent
 class VideoProvides {
     @Provides
     fun provideVideoDao(appDatabase: AppDatabase): VideoDao = appDatabase.videoDao()
+
+    @Provides
+    fun provideVideoRepository(
+        dataRepository: VideoDataRepository,
+        mockDataRepository: VideoMockDataRepository
+    ): VideoRepository {
+        return if (BuildConfig.IS_LOCAL_ENV) {
+            mockDataRepository
+        } else {
+            dataRepository
+        }
+    }
 }
