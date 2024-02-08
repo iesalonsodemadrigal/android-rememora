e0d954b91d7d31d119dd03e3519c80d98acffbbc
Ibra <brahim.kor@educa.jcyl.es>
2024-02-08 20:40:16 +0200

    Feature:  Incluir un menu inicial que ocupa toda la pantalla.
    He hecho todo muy rapido, en el xml hay muchos atributos que sobran

--- app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -3,9 +3,6 @@ package com.iesam.rememora.features.home.presentation
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
-import com.iesam.rememora.R
 import com.iesam.rememora.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,31 +18,31 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupView()
+        //setupView()
     }
 
-    private fun setupView() {
-        binding.apply {
-            actionImage.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_imagen)
-            }
-            actionVideo.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_video)
-            }
-            actionMusica.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_music)
-            }
-            actionAudio.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_audio)
-            }
-            actionAccount.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_logout)
-            }
-        }
-    }
+//    private fun setupView() {
+//        binding.apply {
+//            actionImage.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_imagen)
+//            }
+//            actionVideo.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_video)
+//            }
+//            actionMusica.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_music)
+//            }
+//            actionAudio.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_audio)
+//            }
+//            actionAccount.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_logout)
+//            }
+//        }
+//    }
 }
