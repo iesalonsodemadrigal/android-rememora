a879d45c41b211e0cd0b767e0a26e5142c49d722
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2024-02-08 20:31:58 +0100

    Incluir un repositorio mockeado en música. (#67)

--- app/src/main/java/com/iesam/rememora/features/music/data/local/MappersLocal.kt
+++ app/src/main/java/com/iesam/rememora/features/music/data/local/MappersLocal.kt
@@ -1,17 +1,17 @@
 package com.iesam.rememora.features.music.data.local
 
-import com.iesam.rememora.features.music.domain.Music
+import com.iesam.rememora.features.music.domain.Song
 
-fun Music.toEntity(time: Long): MusicEntity =
+fun Song.toEntity(time: Long): MusicEntity =
     MusicEntity(this.id, this.title, this.description, this.source, time)
 
-fun List<Music>.toEntity(time: Long): List<MusicEntity> = this.map {
+fun List<Song>.toEntity(time: Long): List<MusicEntity> = this.map {
     it.toEntity(time)
 }
 
-fun MusicEntity.toDomain(): Music =
-    Music(this.id, this.description, this.source, this.title)
+fun MusicEntity.toDomain(): Song =
+    Song(this.id, this.description, this.source, this.title)
 
-fun List<MusicEntity>.toDomain(): List<Music> = this.map {
+fun List<MusicEntity>.toDomain(): List<Song> = this.map {
     it.toDomain()
 }
--- app/src/main/java/com/iesam/rememora/features/music/data/remote/MusicRemoteDataSource.kt
+++ app/src/main/java/com/iesam/rememora/features/music/data/remote/MusicRemoteDataSource.kt
@@ -1,12 +1,13 @@
 package com.iesam.rememora.features.music.data.remote
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.iesam.rememora.app.Either
 import com.iesam.rememora.app.domain.ErrorApp
 import com.iesam.rememora.app.left
 import com.iesam.rememora.app.right
-import com.iesam.rememora.features.music.domain.Music
+import com.iesam.rememora.features.music.domain.Song
 import kotlinx.coroutines.tasks.await
 import java.net.ConnectException
 import java.net.SocketTimeoutException
@@ -15,23 +16,28 @@ import javax.inject.Inject
 
 class MusicRemoteDataSource @Inject constructor(
     private val fireBaseDB: FirebaseDatabase,
-    private val fireBaseStorage: FirebaseStorage
+    private val fireBaseStorage: FirebaseStorage,
+    private val auth: FirebaseAuth
 ) {
 
-    suspend fun obtainMusicList(uid: String): Either<ErrorApp, List<Music>> {
+    suspend fun obtainMusicList(): Either<ErrorApp, List<Song>> {
         return try {
-            val dataSnapshot = fireBaseDB
-                .getReference("users/${uid}/music/playlist1")
-                .get()
-                .await()
-            dataSnapshot.children.map {
-                it.getValue(MusicDBModel::class.java)!!
-            }.map { music ->
-                music.source =
-                    fireBaseStorage.getReferenceFromUrl(music.source!!).downloadUrl.await()
-                        .toString()
-                music.toModel()
-            }.right()
+            auth.uid?.let { uid ->
+                val dataSnapshot = fireBaseDB
+                    .getReference("users/${uid}/music/playlist1")
+                    .get()
+                    .await()
+                dataSnapshot.children.map {
+                    it.getValue(MusicDBModel::class.java)!!
+                }.map { music ->
+                    music.source =
+                        fireBaseStorage.getReferenceFromUrl(music.source!!).downloadUrl.await()
+                            .toString()
+                    music.toModel()
+                }.right()
+            } ?: run {
+                ErrorApp.SessionError.left()
+            }
         } catch (ex: ConnectException) {
             ErrorApp.InternetError.left()
         } catch (ex: UnknownHostException) {
