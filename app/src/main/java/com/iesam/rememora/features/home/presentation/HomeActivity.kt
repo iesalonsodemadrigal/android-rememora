9fbfe4ef708ba93c6e4fa7b0b21ebf4d0878f758
Chema <josem.perram@educa.jcyl.es>
2024-02-09 02:24:04 +0100

    Rediseño del Menú inicial (#71)
    Co-authored-by: Ibra <brahim.kor@educa.jcyl.es>
    Co-authored-by: Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>

--- app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -3,9 +3,6 @@ package com.iesam.rememora.features.home.presentation
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
-import com.iesam.rememora.R
 import com.iesam.rememora.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,37 +12,36 @@ class HomeActivity : AppCompatActivity() {
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupView()
+        //setupView()
     }
 
-    private fun setupView() {
-        binding.apply {
-            actionImage.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_imagen)
-            }
-            actionVideo.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_video)
-            }
-            actionMusica.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_music)
-            }
-            actionAudio.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_audio)
-            }
-            actionAccount.setOnClickListener {
-                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
-                    .navigate(R.id.fragment_logout)
-            }
-        }
-    }
+//    private fun setupView() {
+//        binding.apply {
+//            actionImage.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_imagen)
+//            }
+//            actionVideo.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_video)
+//            }
+//            actionMusica.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_music)
+//            }
+//            actionAudio.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_audio)
+//            }
+//            actionAccount.setOnClickListener {
+//                Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+//                    .navigate(R.id.fragment_logout)
+//            }
+//        }
+//    }
 }
