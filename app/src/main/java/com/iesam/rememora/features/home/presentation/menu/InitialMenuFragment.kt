0bf4c9634c4f0367d793afefb6eeb47ca198c8b0
Sofia <sofia.jimlap@educa.jcyl.es>
2024-02-18 23:22:06 +0100

    feature 75: tutorial

--- app/src/main/java/com/iesam/rememora/features/home/presentation/menu/InitialMenuFragment.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/menu/InitialMenuFragment.kt
@@ -1,17 +1,14 @@
 package com.iesam.rememora.features.home.presentation.menu
 
-import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.getkeepsafe.taptargetview.TapTarget
 import com.getkeepsafe.taptargetview.TapTargetView
 import com.iesam.rememora.R
 import com.iesam.rememora.app.extensions.createTarget
-import com.iesam.rememora.app.extensions.show
 import com.iesam.rememora.databinding.FragmentInitialMenuBinding
 import com.iesam.rememora.features.home.presentation.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -33,30 +30,7 @@ class InitialMenuFragment : Fragment() {
     }
 
     private fun setupView() {
-        val targetPhoto = binding.photoSection.createTarget("PHOTO", "")
-        val targetVideo = binding.videoSection.createTarget("VIDEO", "")
-        val targetMusic = binding.musicSection.createTarget("MUSICA", "")
-        val targetAudio = binding.audioSection.createTarget("AUDIO", "")
-
-        TapTargetView.showFor(activity, targetPhoto,
-            object : TapTargetView.Listener() {
-                override fun onTargetClick(view: TapTargetView) {
-                    super.onTargetClick(view)
-                    TapTargetView.showFor(activity,  targetVideo, object : TapTargetView.Listener() {
-                        override fun onTargetClick(view: TapTargetView) {
-                            super.onTargetClick(view)
-                            TapTargetView.showFor(activity,  targetMusic, object : TapTargetView.Listener() {
-                                override fun onTargetClick(view: TapTargetView) {
-                                    super.onTargetClick(view)
-                                    TapTargetView.showFor(activity, targetAudio )
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-
-
+        tutorial()
         binding.apply {
             (requireActivity() as HomeActivity).hideHomeButton()
             actionAudio.setOnClickListener {
@@ -80,6 +54,44 @@ class InitialMenuFragment : Fragment() {
                 )
             }
         }
+    }
+
+    private fun tutorial() {
+        val targetPhoto = binding.photoSection.createTarget(
+            getString(R.string.tutorial_title_photo),
+            getString(R.string.tutorial_description_photo), 130
+        )
+        val targetVideo = binding.videoSection.createTarget(
+            getString(R.string.tutorial_title_video),
+            getString(R.string.tutorial_description_video), 140
+        )
+        val targetMusic = binding.musicSection.createTarget(
+            getString(R.string.tutorial_title_music),
+            getString(R.string.tutorial_description_music), 140
+        )
+        val targetAudio = binding.audioSection.createTarget(
+            getString(R.string.tutorial_title_audio),
+            getString(R.string.tutorial_description_audio), 140
+        )
+
+        TapTargetView.showFor(activity, targetPhoto, object : TapTargetView.Listener() { //fotos
+                override fun onTargetClick(view: TapTargetView) {
+                    super.onTargetClick(view)
+                    TapTargetView.showFor(activity, targetVideo, object : TapTargetView.Listener() { //videos
+                        override fun onTargetClick(view: TapTargetView) {
+                            super.onTargetClick(view)
+                            TapTargetView.showFor(activity, targetMusic, object : TapTargetView.Listener() { //musica
+                                    override fun onTargetClick(view: TapTargetView) {
+                                        super.onTargetClick(view)
+                                        TapTargetView.showFor(activity, targetAudio) //audio
+                                    }
+                                }
+                            )
+                        }
+                    })
+                }
+            }
+        )
     }
 
 }
