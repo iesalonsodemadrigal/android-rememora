3b0054bc226649de46a8d842595f6136673f5adb
Sofia <sofia.jimlap@educa.jcyl.es>
2024-04-15 21:50:53 +0200

    Intento deep link

--- app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/iesam/rememora/features/home/presentation/HomeActivity.kt
@@ -3,13 +3,15 @@ package com.iesam.rememora.features.home.presentation
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import com.getkeepsafe.taptargetview.TapTargetView
 import com.iesam.rememora.R
 import com.iesam.rememora.app.extensions.createTarget
 import com.iesam.rememora.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
@@ -24,6 +26,31 @@ class HomeActivity : AppCompatActivity() {
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+
+        if (intent !== null) {
+            handleIntent(intent)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        if (intent !== null) {
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val uri : Uri? = intent.data
+        val action: String? = intent.action
+
+        Log.d("sof", action!!)
+        Log.d("sof", uri.toString())
+
+        if (uri.toString() == "rememora://images" && action == "android.intent.action.VIEW"){
+            Navigation.findNavController(this@HomeActivity, R.id.fragment_container)
+                .navigate(R.id.fragment_imagen)
+        }
     }
 
     private fun setupView() {
@@ -52,7 +79,7 @@ class HomeActivity : AppCompatActivity() {
         //tutorial()
     }
 
-    private fun tutorial(){
+    private fun tutorial() {
         val targetPhoto = binding.actionImage.createTarget(
             getString(R.string.tutorial_title_photo),
             getString(R.string.tutorial_description_photo), 100
