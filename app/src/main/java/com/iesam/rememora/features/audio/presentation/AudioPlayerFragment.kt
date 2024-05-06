9d189a199ae08136302f79171e373b0473fed2fa
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-05-06 18:33:08 +0200

    Respuesta de voz desde vídeos, música y audios (#108)

--- app/src/main/java/com/iesam/rememora/features/audio/presentation/AudioPlayerFragment.kt
+++ app/src/main/java/com/iesam/rememora/features/audio/presentation/AudioPlayerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.iesam.rememora.R
 import com.iesam.rememora.app.extensions.hide
 import com.iesam.rememora.app.extensions.show
 import com.iesam.rememora.app.presentation.error.ErrorUiModel
@@ -25,7 +26,7 @@ class AudioPlayerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAudioBinding.inflate(inflater, container, false)
-        binding.mediaPlayer.setFragment(this)
+        binding.mediaPlayer.setFragment(this, getString(R.string.fragment_name_audio))
         return binding.root
     }
 
